fff46dc4987f11c6650f5d53a64e9e0db7932af3
Al-Kom <alexarkom99@gmail.com>
2019-10-05 19:11:02 +0300

    Update initialisation test

--- main.c
+++ main.c
@@ -5,20 +5,23 @@
 int main()
 {
     printf("Hello world!\n");
-
     //Utests
-    printf("Test initialize()\n");
-    int n=5,szp=80,max_val=2147483647;
-    test_init(n,szp);
-    test_init(-n,szp);
-    test_init(-n,-szp);
-    test_init(n,0);
-    test_init(max_val,max_val);
-    test_init(max_val+1,max_val);
+	test_init();
 
     return 0;
 }
 
-void test_init(int n,int szp) {
+void test_init() {
+    printf("\nTest initialize()\n");
+    int n=5,szp=80,max_val=2147483647;
+    print_test_init(n,szp);
+    print_test_init(-n,szp);
+    print_test_init(-n,-szp);
+    print_test_init(n,0);
+    print_test_init(max_val,max_val);
+    print_test_init(max_val+1,max_val);
+}
+
+void print_test_init(int n,int szp) {
     printf("#initialize(%d,%d) return %d\n",n,szp,initialize(n,szp));
 }
